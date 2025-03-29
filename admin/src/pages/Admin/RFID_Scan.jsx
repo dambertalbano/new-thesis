75d9ecc95655a7ef803029a940a4d4f76a963998
Oda <you@example.com>
2025-03-30 04:08:52 +0800

    updated

--- admin/src/pages/Admin/RFID_Scan.jsx
+++ admin/src/pages/Admin/RFID_Scan.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { FiInfo } from "react-icons/fi";
 import blankImage from "../../assets/blank-image.webp";
 import { AdminContext } from "../../context/AdminContext";
+import { Loader } from "lucide-react";
 
 const RFID_Scan = () => {
     const { getUserByCode, adminSignIn, adminSignOut } = useContext(AdminContext);
@@ -10,6 +11,10 @@ const RFID_Scan = () => {
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
     const [lastScannedTimes, setLastScannedTimes] = useState({}); // Store last scanned time per user
+
+    useEffect(() => {
+        document.title = 'Scan';
+    }, []);
 
     useEffect(() => {
         const handleKeyDown = (event) => {
@@ -109,7 +114,7 @@ const RFID_Scan = () => {
     };
 
     const formatName = (user) => {
-        if (!user) return 'N/A';
+        if (!user) return 'No Data';
         const lastName = user.lastName || '';
         const firstName = user.firstName || '';
         const middleName = user.middleName ? `${user.middleName.charAt(0)}.` : '';
@@ -151,12 +156,17 @@ const RFID_Scan = () => {
     return (
         <div className="flex justify-center items-center min-h-screen w-full bg-gray-100 p-4">
             <div className="bg-white p-8 rounded-xl shadow-lg text-center max-w-2xl w-full">
-                <div className="flex items-center justify-center mb-4 text-customRed">
-                    <FiInfo className="w-8 h-8" />
-                    <h2 className="text-3xl font-semibold ml-2">User Information</h2>
-                </div>
+                {!loading && (
+                    <div className="flex items-center justify-center mb-4 text-customRed">
+                        <FiInfo className="w-8 h-8" />
+                        <h2 className="text-3xl font-semibold ml-2">User Information</h2>
+                    </div>
+                )}
                 {loading ? (
-                    <p className="text-blue-500 text-center">⏳ Scanning...</p>
+                    <div className="flex justify-center items-center">
+                    <Loader className="w-5 h-5 text-customRed animate-spin mr-2" />
+                    <span className="text-customRed">Scanning ...</span>
+                  </div>
                 ) : error ? (
                     <>
                         <p className="text-red-500 text-center">{error}</p>
@@ -183,7 +193,7 @@ const RFID_Scan = () => {
 const UserInfoDisplay = ({ userInfo, formatName }) => {
     // Function to format the date and time
     const formatDateTime = (dateTimeString) => {
-        if (!dateTimeString) return 'N/A';
+        if (!dateTimeString) return 'No Data';
         try {
             const date = new Date(dateTimeString);
             return date.toLocaleDateString('en-US', {
